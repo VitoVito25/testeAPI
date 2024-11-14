584b1cef0394ab50299fced5552251f75297977b
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-11-14 10:29:07 -0300

    conexão bem sucedida

--- teste.py
+++ teste.py
@@ -4,6 +4,8 @@ connection_data = (
     "Driver={PostgreSQL Unicode};"
     "Server=PAT-1575;Port=5432;"
     "Database=organizeiTeste;"
+    "Uid=postgres;"
+    "Pwd=masterkey;"
 )
 
 connection = pyodbc.connect(connection_data)
